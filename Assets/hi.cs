7191e534c0cfdb9107c0e7140bedc9e4c3bdf6a9
StaticGamer881 <vishrut172@gmail.com>
2020-09-17 13:50:04 +0530

    the game
    wdd

---
+++ Assets/hi.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hi : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer g_plane;
+
+    void Update()
+    {
+        g_plane.material.color = Color.red;
+    }
+
+}
