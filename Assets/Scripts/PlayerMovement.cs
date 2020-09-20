1bcb5a0eb59fcc6df8a4db3d1a5cf2498bebafb7
StaticGamer881 <vishrut172@gmail.com>
2020-09-20 22:18:13 +0530

    this is what i have done so far

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private bool start;
+    [SerializeField] private Camera _camera;
+
+    void Start()
+    {
+        start = true;
+        _camera.fieldOfView = 0f;
+    }
+
+    void Update()
+    {
+        if (start)
+        {
+            _camera.fieldOfView = Mathf.Lerp(_camera.fieldOfView, 60, Time.deltaTime * 0.5f);
+            StartCoroutine(startRoutine(5));
+        }
+    }
+
+    private IEnumerator startRoutine(int seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        _camera.fieldOfView = 60f;
+        start = false;
+    }
+}
